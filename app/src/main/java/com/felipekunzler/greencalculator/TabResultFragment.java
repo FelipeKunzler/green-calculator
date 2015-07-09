49d3d39c20d5584b289e32d184841789866ed7cb
Felipe <felipekunzler@gmail.com>
2015-07-09 22:36:03 +0100

    Implemented result view

--- app/src/main/java/com/felipekunzler/greencalculator/TabResultFragment.java
+++ app/src/main/java/com/felipekunzler/greencalculator/TabResultFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 
 /**
  * Created by Felipe on 07/07/2015.
@@ -15,6 +16,7 @@ public class TabResultFragment extends  Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.tab_result,container,false);
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
         return v;
     }
 }
